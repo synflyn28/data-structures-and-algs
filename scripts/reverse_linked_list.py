2a46441f8d3aedfb2c213fb5a60ce155cae2d68f
Justin Reid <jlreid28@Mac.MG8702>
2025-03-07 09:17:28 -0500

    Added LinkedList implementation.

---
+++ scripts/reverse_linked_list.py
@@ -0,0 +1,29 @@
+"""
+This script contains the implementation of a reversing a linked list.
+"""
+
+class Solution:
+    def reverse_list(self, head: ListNode) -> ListNode:
+        """
+        Reverse a linked list. Starts at a Node instead of a LinkedList instance
+        since it has to iterate through all the node next values.
+        """
+
+        # Three pointers used to iterate and reverse the linked list.
+        pre = None
+        cur = head
+        suc = None
+
+        while cur:
+            suc = cur.next
+            cur.next = pre # *Switches the reference of the current node to reverse the LinkedList*
+            
+            # Move pre and cur pointers one step forward.
+            pre = cur
+            cur = suc
+            # No succeeding move needed because suc is assigned next value at beginning of loop.
+
+            return pre # Return preceding node when cur is None at the end of the LinkedList.
+
+
+            
